a4a89814b6b5a0916ab7a56078f60915cf3277d4
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-06-18 08:39:22 +0300

    add dotenv

--- firebase/config.js
+++ firebase/config.js
@@ -1,5 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // Функція для підключення авторизації в проект
 import { getAuth } from "firebase/auth";
 // Функція для підключення бази даних у проект
@@ -7,15 +8,28 @@ import { getFirestore } from "firebase/firestore";
 // Функція для підключення сховища файлів в проект
 import { getStorage } from "firebase/storage";
 
+
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCmjEenxpsTfWUHgv1A6bwSkMCNO_-xPCQ",
-  authDomain: "goit-rnative.firebaseapp.com",
-  databaseURL: "https://goit-rnative.firebaseio.com",
-  projectId: "goit-rnative",
-  storageBucket: "goit-rnative.appspot.com",
-  messagingSenderId: "774413772887",
-  appId: "1:774413772887:web:cf4ce84cab7b9df93b1b12",
-  measurementId: "G-measurement-id",
+
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
